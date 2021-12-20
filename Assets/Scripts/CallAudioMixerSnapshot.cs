7de6274530cd35bf576219e0eb1e6964f34a9671
Matias Harju <matias.harju@iki.fi>
2021-12-20 18:59:26 +0200

    add openxr support, add choice sequence, add start button and mockup subtitle buttons

---
+++ Assets/Scripts/CallAudioMixerSnapshot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class CallAudioMixerSnapshot : MonoBehaviour
+{
+
+    public AudioMixerSnapshot snapshot;
+    public float fadeTime = 0.0f;
+    public bool callOnAwake = true;
+
+    void Awake()
+    {
+        if (callOnAwake)
+        {
+            snapshot.TransitionTo(fadeTime);
+            Debug.Log(snapshot + " called with fade time " + fadeTime);
+        }
+    }
+
+}
