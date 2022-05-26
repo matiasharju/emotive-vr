288f16b6a169d58f93ef4798467657eec9011442
Matias Harju <matias.harju@iki.fi>
2022-05-26 17:20:55 +0300

    cigar burn effect for prologue

--- Assets/Scripts/Cigar.cs
+++ Assets/Scripts/Cigar.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Cigar : MonoBehaviour
 {
@@ -12,13 +13,17 @@ public class Cigar : MonoBehaviour
     private float _time = 0;
     private int _indexTime = 0;
 
-    // Start is called before the first frame update
+    public Color colorIdle;
+    public Color colorInhale;
+
     void Start()
     {
         _burnMat = GetComponent<MeshRenderer>().material;
+        _burnMat.color = colorIdle;
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         if(_indexTime < timestampCigare.Count)
@@ -28,17 +33,41 @@ public class Cigar : MonoBehaviour
             if (_time >= timestampCigare[_indexTime])
             {
                 _indexTime++;
-                _burnMat.SetInt("_Bool_Burn", 0);
-                _burnMat.SetFloat("_Mask_Range", maxBurn);
-                StartCoroutine(CO_Wait());
+                StartCoroutine(Inhale());
+//                _burnMat.SetInt("_Bool_Burn", 0);
+//                _burnMat.SetFloat("_Mask_Range", maxBurn);
+//                StartCoroutine(CO_Wait());
                 
             }
         }
     }
 
+    IEnumerator Inhale()
+    {
+        float a = 0.0f;
+        while (a < 1)
+        {
+            _burnMat.color = Color.Lerp(colorIdle, colorInhale, a);
+            Debug.Log(_burnMat.color);
+            a = a + 0.05f;
+            yield return new WaitForSeconds(0.05f);
+        }
+        
+        yield return new WaitForSeconds(1.5f);
+
+        while (a > 0.0f)
+        {
+            _burnMat.color = Color.Lerp(colorIdle, colorInhale, a);
+            a = a - 0.05f;
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        //        _burnMat.color = colorIdle;
+    }
+
     IEnumerator CO_Wait()
     {
         yield return new WaitForSeconds(3);
-        _burnMat.SetInt("_Bool_Burn", 1);
+//        _burnMat.SetInt("_Bool_Burn", 1);
     }
 }
