792056cd3321a737a165acfb6a64298aea3f67b3
Matias Harju <matias.harju@iki.fi>
2022-05-27 14:05:43 +0300

    add ignition to cigar

--- Assets/Scripts/Cigar.cs
+++ Assets/Scripts/Cigar.cs
@@ -13,13 +13,14 @@ public class Cigar : MonoBehaviour
     private float _time = 0;
     private int _indexTime = 0;
 
+    public Color colorDark;
     public Color colorIdle;
     public Color colorInhale;
 
     void Start()
     {
         _burnMat = GetComponent<MeshRenderer>().material;
-        _burnMat.color = colorIdle;
+        _burnMat.color = colorDark;
 
     }
 
@@ -42,6 +43,27 @@ public class Cigar : MonoBehaviour
         }
     }
 
+    IEnumerator Ignite()
+    {
+        float a = 0.0f;
+        while (a < 1)
+        {
+            _burnMat.color = Color.Lerp(colorDark, colorInhale, a);
+            Debug.Log(_burnMat.color);
+            a = a + 0.1f;
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        yield return new WaitForSeconds(1.5f);
+
+        while (a > 0.0f)
+        {
+            _burnMat.color = Color.Lerp(colorIdle, colorInhale, a);
+            a = a - 0.05f;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
     IEnumerator Inhale()
     {
         float a = 0.0f;
@@ -61,8 +83,6 @@ public class Cigar : MonoBehaviour
             a = a - 0.05f;
             yield return new WaitForSeconds(0.05f);
         }
-
-        //        _burnMat.color = colorIdle;
     }
 
     IEnumerator CO_Wait()
